163bce47d1e500fc6a080d4f5dee3accb2db2638
Leigh MacDonald <leigh.macdonald@gmail.com>
2025-09-07 21:21:56 -0600

    Remove extra text

--- test/SteamIDSpec.hs
+++ test/SteamIDSpec.hs
@@ -1,12 +1,8 @@
 module SteamIDSpec (spec) where
 import Test.Hspec
--- import Control.Exception (evaluate)
--- import SteamID (parseSteamID)
-
 
 spec :: Spec
 spec = do
     describe "SteamID" $ do
         it "should parse valid SteamIDs" $ do
            (True) `shouldBe` (True)
-git remote add origin git@github.com:leighmacdonald/steamid-hs.git
